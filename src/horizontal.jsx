cb43be4cbcd66050359de0a506f5e9887d3278b2
sarangpatil2905 <113379047+sarangpatil2905@users.noreply.github.com>
2025-04-22 09:35:54 +0530

    Add files via upload
    SLIDINGS

---
+++ src/horizontal.jsx
@@ -0,0 +1,53 @@
+import './horizontal.css'
+
+export default function horizontal() {
+    return (
+        <section>
+            <div class="carousel-wrapper">
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+    );
+}
---
+++ src/vertical.jsx
@@ -0,0 +1,52 @@
+import './vertical.css'
+
+export default function vertical() {
+    return (
+        <section>
+            <div class="carousel-wrapper">
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+                <div class="carousel-container">
+                    <div class="carousel">
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                        <div class="classs">Item 1</div>
+                        <div class="classs">Item 2</div>
+                        <div class="classs">Item 3</div>
+                        <div class="classs">Item 4</div>
+                        <div class="classs">Item 5</div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
