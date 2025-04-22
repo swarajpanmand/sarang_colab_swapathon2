d6be63447fb1105fbae710b41f553d8030ac6b5e
Swaraj Panmand <swarajpanmand@gmail.com>
2025-04-22 09:07:56 +0530

    initial commit

---
+++ src/animation/base.jsx
@@ -0,0 +1,11 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Robo() {
+  return (
+    <main>
+        <div className="absolute inset-2 w-[100%] h-[100%]">
+
+      </div>
+    </main>
+  );
+}
---
+++ src/animation/page/home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+         <div className="w-screen h-screen">
+            <div className="relative inset-0 w-screen h-screen bg-black">
+                <Aurora/>
+            </div>
+            <Robo/>
+      </div>
+    </>
+  )
+}
+
+export default Home
